1d9c2059566ec4bde28a33c4f2757b648102b70d
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 21:50:32 +0800

    Update knowledge_entity.py

--- service_knowledge_manage/entity/knowledge_entity.py
+++ service_knowledge_manage/entity/knowledge_entity.py
@@ -1,10 +1,4 @@
 from sqlalchemy import Float
-"""
-@File         :knowledge_entity.py
-@Description  :
-@Author       :QiangQu
-@Date         :2024/09/03 15:14:54
-"""
 
 from typing import Optional
 from pydantic import BaseModel, Field

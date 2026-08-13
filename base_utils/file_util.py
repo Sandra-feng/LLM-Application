08bac36920a9925e6e27c590b727b6fcdafa5a2e
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 21:43:07 +0800

    Remove unnecessary comments and clean up code

--- base_utils/file_util.py
+++ base_utils/file_util.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-"""
-@Project ：tiance-base 
-@File    ：file_util.py.py
-@Author  ：TaoMeng
-@Date    ：2024/8/25 20:19 
-"""
+
 import os
 import json
 import shutil

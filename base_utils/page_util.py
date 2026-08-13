f48d86f4382caf87b98179c7823832ced350b384
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 21:35:23 +0800

    Refactor PageUtil class and clean up comments
    Removed unnecessary comments and cleaned up the code.

--- base_utils/page_util.py
+++ base_utils/page_util.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-'''
-@File         :list_util.py
-@Description  :
-@Author       :QiangQu
-@Date         :2024/09/03 09:50:30
-'''
+
 
 class PageUtil(object):
     """

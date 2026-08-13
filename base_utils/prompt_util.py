963783a9baaae2e0421d80d07718fad0d90925b3
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 21:33:48 +0800

    Refactor PromptUtil class formatting method

--- base_utils/prompt_util.py
+++ base_utils/prompt_util.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-"""
-@Project ：tiance-base 
-@File    ：prompt_util.py.py
-@Author  ：TaoMeng
-@Date    ：2024/8/25 20:22 
-"""
+
 
 
 class PromptUtil(object):

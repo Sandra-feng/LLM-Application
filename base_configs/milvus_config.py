f8f60675a05f64e54daa5241d8f36cbad40b46c4
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 21:40:53 +0800

    Refactor MilvusConfig class and remove comments

--- base_configs/milvus_config.py
+++ base_configs/milvus_config.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-"""
-@Project ：tiance-base
-@File    ：milvus_config.py
-@Author  ：TaoMeng
-@Date    ：2024/8/25 22:39
-"""
 
 from base_configs.api_config import ApiConfig
 

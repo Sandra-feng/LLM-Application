67f5a25a27ef058bbd163490db13eacb96b35d75
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 21:48:17 +0800

    Update config_manage_api.py

--- service_config_manage/api/config_manage_api.py
+++ service_config_manage/api/config_manage_api.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-"""
-@Project ：tiance-base
-@File    ：config_manage_api.py
-@Author  ：YunPeng
-@Date    ：2024/8/29 14.17
-"""
+
 from base_configs.api_config import ApiConfig
 from fastapi import APIRouter
 from service_config_manage.api.routes import get_model_info_route, file_upload_download_route
